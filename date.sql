5f8067481b57c08c7382499cb0e50a56ee402c53
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-21 00:07:41 +0600

    select now date and time

--- date.sql
+++ date.sql
@@ -1,3 +1,18 @@
+-- Active: 1747418280166@@localhost@5432@postgres
 --show timezone;
 
 SELECT now()
+
+CREATE TABLE timeZ (ts TIMESTAMP without time zone,tsz TIMESTAMP with time zone)
+
+Insert into timeZ VALUES('2025-05-20 10:45:00', '2025-05-20 10:45:00')
+
+SELECT * from timeZ 
+
+SELECT now()
+
+SELECT CURRENT_DATE;
+
+SELECT now()::date;
+
+SELECT now()::time;
