7f7db9ec59942eda40830878e4540afd1a1722b8
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-21 20:39:05 +0600

    inner join learning

---
+++ join.sql
@@ -0,0 +1,36 @@
+CREATE Table "user"(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+)
+
+CREATE Table post(
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+)
+
+
+
+INSERT INTO "user" (username) VALUES
+('akash'),
+('batash'),
+('sagor'),
+('nodi');
+
+INSERT INTO post (title, user_id) VALUES
+('Enjoying a sunny day with Akash! ☀️', 2),
+('Batash just shared an amazing recipe! 🍲', 1),
+('Exploring adventures with Sagor.🌟', 4),
+('Nodi''s wisdom always leaves me inspired. 📚', 4);
+
+
+DROP Table post;
+DROP Table "user";
+
+SELECT * from "user";
+SELECT * from post;
+
+SELECT title, username FROM post
+JOIN "user" on post.user_id = "user".id
+
+-- inner join
